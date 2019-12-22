bc0fa8668db866d8c791c79bca91a87161190dc4
mhoffman <mike@michaelhoffmaninc.com>
2019-12-22 07:14:24 -0600

    Added use of config property annotation from microprofile

--- src/main/java/com/michaelhoffmantech/quarkus/ordermanagement/OrderManagement.java
+++ src/main/java/com/michaelhoffmantech/quarkus/ordermanagement/OrderManagement.java
@@ -1,5 +1,7 @@
 package com.michaelhoffmantech.quarkus.ordermanagement;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,10 +11,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/orders")
 public class OrderManagement {
 
+    @ConfigProperty(name = "message.orderProcessed")
+    String orderProcessedMessage;
+
     @GET
     @Path("/{orderId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Order getOrder(@PathParam("orderId") Integer orderId) {
-        return new Order(orderId, "Test Order");
+        return new Order(orderId, orderProcessedMessage);
     }
 }
